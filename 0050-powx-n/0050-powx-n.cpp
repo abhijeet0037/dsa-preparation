64c912b803b07d15bdc90ab45be8a0530373844c
abhijeet0037 <abhijeetrastogi2006@gmail.com>
2026-08-03 16:58:05 +0530

    Time: 0 ms (100%), Space: 8.5 MB (93.47%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,26 +1,17 @@
 class Solution {
 public:
     double myPow(double x, int n) {
-        if(n==0) return 1.0;
-        if(x==0) return 0.0;
-        if(x==1) return 1.0;
-        if(x==-1 && n%2 == 0 ) return 1.0;
-        if(x==-1 && n%2 != 0) return -1.0;
-
-        long binForm = n;
+         long binaryform=n;
         if(n<0){
-            x = 1/x;
-            binForm = -binForm;
+            x=1/x;
+            binaryform=-binaryform;
         }
-
-        double ans = 1;
-        while(binForm>0){
-            if(binForm % 2 == 1){
-                ans *= x;
-        }
+      double ans=1.0;
+      while(binaryform>0){
+        if(binaryform%2==1)ans=ans*x;
         x*=x;
-        binForm /=2;
-    }
-    return ans;
+        binaryform=binaryform/2;
+      }
+      return ans;
     }
 };
